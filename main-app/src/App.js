42e0c818d04fea96a78fe6b8d717fd826ab02607
Dan Li <danmuxingli@gmail.com>
2023-06-17 19:07:48 -0400

    create react app for bot dialog

---
+++ main-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './images/logo.svg';
+import './styles/App.css';
+import MainDialog from './components/BotDisplay';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MainDialog />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ main-app/src/components/BotDisplay.js
@@ -0,0 +1,19 @@
+import Chatbot from "react-chatbot-kit";
+import "react-chatbot-kit/build/main.css";
+import config from "../chatbot/config";
+import MessageParser from "../chatbot/MessageParser";
+import ActionProvider from "../chatbot/ActionProvider";
+
+const MainDialog = () => {
+    return (
+        <div>
+            <Chatbot
+                config={config}
+                messageParser={MessageParser}
+                actionProvider={ActionProvider}
+            />
+        </div>
+    );
+};
+
+export default MainDialog;
