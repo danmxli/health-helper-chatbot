307451c06189adfeaf48572bddecf40106db6086
Dan Li <danmuxingli@gmail.com>
2023-09-01 17:34:25 -0400

    Create Display Config for NLP widget

--- main-app/src/components/widgets/DisplayConfig.jsx
+++ main-app/src/components/widgets/DisplayConfig.jsx
@@ -1,18 +1,42 @@
 import React, { useEffect, setState } from 'react';
-import handleConfig from '../../functions/HandleConfig';
 import '../../styles/DisplayConfig.css';
 
 const DisplayConfig = (props) => {
     const { setState } = props;
 
+    useEffect(() => {
+        // fetch 
+        fetch('http://localhost:8080/config')
+            .then(response => response.json())
+            .then(data => {
+                if (data && data.data) {
+                    console.log(data);
+                    setState((state) => ({ ...state, displayConfig: data.data }));
+                }
+            })
+    }, [setState])
+
     const renderDefinition = () => {
-        const message = "I am your personal healthcare companion."
-        return message;
+        const { displayConfig } = props;
+        if (!displayConfig || displayConfig.length === 0) {
+            return <p>No data to display</p>;
+        }
+
+        return (
+            <div className='config-box-item'>
+                <ul>
+                    {displayConfig.map((item) => (
+                        <li key={item._id}>
+                            name: {item.name}, prompt: {item.prompt}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+        );
     }
 
-    return <div className="config-box-item">
-        {renderDefinition()}
-    </div>;
+    return renderDefinition();
 }
 
 export default DisplayConfig;
