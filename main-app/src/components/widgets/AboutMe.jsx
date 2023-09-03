86fa9ea1449f4feb71063d764c7037fc125b6bd8
Dan Li <danmuxingli@gmail.com>
2023-09-02 22:10:35 -0400

    Update widget css

--- main-app/src/components/widgets/AboutMe.jsx
+++ main-app/src/components/widgets/AboutMe.jsx
@@ -4,13 +4,14 @@ import '../../styles/AboutMe.css'
 const AboutMe = (props) => {
     const { setState } = props;
 
-    const renderDefinition = () => {
-        const message = "I am your personal healthcare companion."
-        return message;
-    }
-
-    return <div className = "about-box-item">
-        {renderDefinition()}
+    return <div className="about-box-item">
+        My name is Heather! I am your personal healthcare companion. Here's what I can do:
+        <li>
+            I can provide health tips to you and your friends.
+        </li>
+        <li>
+            I can provide COVID-19 statistics to keep you updated.
+        </li>
     </div>;
 }
 
